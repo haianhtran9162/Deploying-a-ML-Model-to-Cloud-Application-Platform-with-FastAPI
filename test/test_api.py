69a079c05bfce3e5be7e8678e85d1e5e751357a1
anhth <anhth.fuhn@gmail.com>
2023-09-12 19:14:19 +0700

    Update test case for api testing

--- test/test_api.py
+++ test/test_api.py
@@ -10,11 +10,14 @@ def test_init_client():
         Test the server is still running
     """
     respone = client.get('/')
+    # Test code respone
     assert respone.status_code == 200
+    # Test message respone
+    assert respone.json()[0] == "I am still alive ヾ(⌐■_■)ノ♪"
     
-def test_predict_happy_case():
+def test_predict_happy_case_1():
     """
-        Test model predict with happy case
+        Test model predict with happy case <= 50K
     """
     data_test = {
                 "age": 30,
@@ -33,7 +36,36 @@ def test_predict_happy_case():
                 "native_country": "United-States"
             }
     respone = client.post('/predict', json=data_test)
+    # Check response code
     assert respone.status_code == 200
+    # Check response predict results of the model
+    assert respone.json() == "<=50K" 
+    
+def test_predict_happy_case_2():
+    """
+        Test model predict with happy case <= 50K
+    """
+    data_test = {
+                "age": 32,
+                "workclass": "Private",
+                "fnlgt": 29933,
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Married-civ-spouse",
+                "occupation": "Handlers-cleaners",
+                "relationship": "Husband",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 0,
+                "capital_loss": 0,
+                "hours_per_week": 50,
+                "native_country": "United-States"
+            }
+    respone = client.post('/predict', json=data_test)
+    # Check response code
+    assert respone.status_code == 200
+    # Check response predict results of the model
+    assert respone.json() == ">50K" 
     
 def test_predict_error_case():
     """
