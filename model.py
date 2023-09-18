c81b45e11e41e5a90c19726879729d6f51aa464d
anhth <anhth.fuhn@gmail.com>
2023-09-18 09:07:13 +0700

    add example to schema_extra

--- model.py
+++ model.py
@@ -15,3 +15,23 @@ class InputData(BaseModel):
     capital_loss: int
     hours_per_week: int
     native_country: str
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 30,
+                "workclass": "Private",
+                "fnlgt": 59496,
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Married-civ-spouse",
+                "occupation": "Sales",
+                "relationship": "Husband",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2407,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States"
+            }
+        }
